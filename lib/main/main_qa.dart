b1c83082ea18b0a7fe359bf7a0b507a53f137974
vinc <appleyardvincent@gmail.com>
2024-02-23 04:16:43 +0100

    ios firebase integration

--- lib/main/main_qa.dart
+++ lib/main/main_qa.dart
@@ -3,11 +3,13 @@ import 'package:chefio_recipe_app/config/app_config.dart';
 import 'package:chefio_recipe_app/config/locator/locator.dart';
 import 'package:chefio_recipe_app/config/locator/qa.locator.dart';
 import 'package:chefio_recipe_app/config/providers_wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
     final config = AppConfig(
       appEnvironment: AppEnvironment.qa,
