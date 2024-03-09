61a2806e2e2c5340a6ae73a900db68225cb6dd47
vinc <appleyardvincent@gmail.com>
2024-03-09 07:17:05 +0100

    upload recipe request entity

--- lib/modules/recipe/domain/entities/requests/upload_recipe.request.dart
+++ lib/modules/recipe/domain/entities/requests/upload_recipe.request.dart
@@ -1,1 +1,31 @@
-class UploadRecipeRequest {}
+import 'dart:io';
+
+class UploadRecipeRequest {
+  final File coverPhoto;
+  final String foodName;
+  final String description;
+  final Duration duration;
+  final List<String> ingredients;
+  final List<CreateCookingStepRequest> cookingSteps;
+
+  UploadRecipeRequest({
+    required this.coverPhoto,
+    required this.foodName,
+    required this.description,
+    required this.duration,
+    required this.ingredients,
+    required this.cookingSteps,
+  });
+}
+
+class CreateCookingStepRequest {
+  final int step;
+  final String description;
+  final File? photo;
+
+  CreateCookingStepRequest({
+    required this.step,
+    required this.description,
+    required this.photo,
+  });
+}
