6eb6ac501554d5db94d74a38378df5d378dc51ae
vinc <appleyardvincent@gmail.com>
2024-09-02 05:44:57 +0100

    chore: Add IRecipeListDataSource interface for recipe list retrieval

---
+++ lib/modules/recipe/data/data_sources/interfaces/i_recipe_list_datasource.dart
@@ -0,0 +1,9 @@
+import 'package:chefio_recipe_app/modules/auth/data/models/user.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/models/recipe.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/models/recipe_category.model.dart';
+
+abstract class IRecipeListDataSource {
+  Future<List<RecipeModel>> getRecipes({required RecipeCategoryModel? category});
+  Future<List<RecipeModel>> getRecipesByUser(UserModel user);
+  Future<List<RecipeModel>> getLikedRecipesByUser(UserModel user);
+}
