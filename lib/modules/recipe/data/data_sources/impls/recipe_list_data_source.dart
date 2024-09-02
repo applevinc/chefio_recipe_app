ee791f2c90148b6c678a57a1c3a0e4dcff5a69fd
vinc <appleyardvincent@gmail.com>
2024-09-02 05:59:45 +0100

    feat: Implement RecipeListDataSource for retrieving recipe list data

---
+++ lib/modules/recipe/data/data_sources/impls/recipe_list_data_source.dart
@@ -0,0 +1,93 @@
+import 'dart:developer';
+
+import 'package:chefio_recipe_app/core/models/failure.dart';
+import 'package:chefio_recipe_app/core/models/user.dart';
+import 'package:chefio_recipe_app/core/services/firebase_storage.dart';
+import 'package:chefio_recipe_app/core/services/firestore_collections.dart';
+import 'package:chefio_recipe_app/modules/auth/data/models/user.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/data_sources/interfaces/i_recipe_datasource.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/data_sources/interfaces/i_recipe_list_datasource.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/models/recipe.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/models/recipe_category.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/models/requests/cooking_step_request.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/models/requests/upload_recipe_request.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/domain/entities/requests/cooking_step.request.dart';
+
+class RecipeListDataSource implements IRecipeListDataSource {
+  @override
+  Future<List<RecipeCategoryModel>> getAllCategories() async {
+    try {
+      final querySnapshot = await categoriesCollection.get();
+      return querySnapshot.docs.map((doc) => RecipeCategoryModel.fromJson(doc.data())).toList();
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+
+      throw InternalFailure();
+    }
+  }
+
+  @override
+  Future<List<RecipeModel>> getLikedRecipesByUser(User user) async {
+    return [];
+  }
+
+  @override
+  Future<List<RecipeModel>> getRecipes({required RecipeCategoryModel? category}) async {
+    try {
+      final querySnapshot = await recipesCollection.get();
+      final resultFutures = querySnapshot.docs.map(
+        (doc) async {
+          final data = doc.data();
+          final user = await _getUser(data['user_id']);
+
+          return RecipeModel.fromDataSource(
+            data: doc.data(),
+            user: user,
+            category: category,
+          );
+        },
+      );
+
+      return await Future.wait(resultFutures);
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+
+      throw InternalFailure();
+    }
+  }
+
+  Future<UserModel> _getUser(String userId) async {
+    final querySnapshot = await usersCollection.doc(userId).get();
+    final data = querySnapshot.data();
+
+    if (data == null) {
+      throw Failure('User not found');
+    }
+
+    return UserModel.fromMap(data);
+  }
+
+  @override
+  Future<List<RecipeModel>> getRecipesByUser(User user) async {
+    try {
+      final querySnapshot = await recipesCollection.where('user_id', isEqualTo: user.id).get();
+      final result = querySnapshot.docs.map(
+        (doc) {
+          return RecipeModel.fromDataSource(
+            data: doc.data(),
+            user: user,
+            category: null,
+          );
+        },
+      ).toList();
+      return result;
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+
+      throw InternalFailure();
+    }
+  }
+}
