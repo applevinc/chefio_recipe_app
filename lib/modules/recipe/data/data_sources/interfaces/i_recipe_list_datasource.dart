a6e3e7cc06a663a5d448b0dbf83b7186308387e7
vinc <appleyardvincent@gmail.com>
2024-09-02 06:06:24 +0100

    feat: Update recipe list data sources and repositories

--- lib/modules/recipe/data/data_sources/interfaces/i_recipe_list_datasource.dart
+++ lib/modules/recipe/data/data_sources/interfaces/i_recipe_list_datasource.dart
@@ -1,10 +1,11 @@
+import 'package:chefio_recipe_app/core/models/user.dart';
 import 'package:chefio_recipe_app/modules/auth/data/models/user.model.dart';
 import 'package:chefio_recipe_app/modules/recipe/data/models/recipe.model.dart';
 import 'package:chefio_recipe_app/modules/recipe/data/models/recipe_category.model.dart';
 
 abstract class IRecipeListDataSource {
   Future<List<RecipeModel>> getRecipes({required RecipeCategoryModel? category});
-  Future<List<RecipeModel>> getRecipesByUser(UserModel user);
-  Future<List<RecipeModel>> getLikedRecipesByUser(UserModel user);
+  Future<List<RecipeModel>> getRecipesByUser(User user);
+  Future<List<RecipeModel>> getLikedRecipesByUser(User user);
   Future<List<RecipeCategoryModel>> getAllCategories();
 }
---
+++ lib/modules/recipe/data/repositories/recipe_list_repository.dart
@@ -0,0 +1,37 @@
+import 'package:chefio_recipe_app/core/models/user.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/data_sources/interfaces/i_recipe_datasource.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/data_sources/interfaces/i_recipe_list_datasource.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/models/recipe_category.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/data/models/requests/upload_recipe_request.model.dart';
+import 'package:chefio_recipe_app/modules/recipe/domain/entities/recipe_category.dart';
+import 'package:chefio_recipe_app/modules/recipe/domain/entities/recipe.dart';
+import 'package:chefio_recipe_app/modules/recipe/domain/entities/requests/upload_recipe.request.dart';
+import 'package:chefio_recipe_app/modules/recipe/domain/repositories/i_recipe_list_repository.dart';
+import 'package:chefio_recipe_app/modules/recipe/domain/repositories/i_recipe_repository.dart';
+
+class RecipeListRepository implements IRecipeListRepository {
+  RecipeListRepository({required IRecipeListDataSource dataSource}) : _dataSource = dataSource;
+
+  final IRecipeListDataSource _dataSource;
+
+  @override
+  Future<List<RecipeCategory>> getCategories() async {
+    return await _dataSource.getAllCategories();
+  }
+
+  @override
+  Future<List<Recipe>> getLikedRecipesByUser(User user) async {
+    return await _dataSource.getLikedRecipesByUser(user);
+  }
+
+  @override
+  Future<List<Recipe>> getRecipes({required RecipeCategory? category}) async {
+    final categoryModel = category == null ? null : RecipeCategoryModel.fromEntity(category);
+    return await _dataSource.getRecipes(category: categoryModel);
+  }
+
+  @override
+  Future<List<Recipe>> getRecipesByUser(User user) async {
+    return await _dataSource.getRecipesByUser(user);
+  }
+}
