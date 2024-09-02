9d24ccfe1f9f3efee3d869ff5e73a6d74ebe399d
vinc <appleyardvincent@gmail.com>
2024-09-02 07:31:26 +0100

    feat: Add FakeRecipeListDataSource for recipe list data retrieval

--- lib/modules/recipe/data/data_sources/fakes/fake_search_recipe_data_source.dart
+++ lib/modules/recipe/data/data_sources/fakes/fake_search_recipe_data_source.dart
@@ -1,3 +1,4 @@
+import 'package:chefio_recipe_app/modules/recipe/data/data_sources/fakes/fake_recipe_list_data_source.dart';
 import 'package:chefio_recipe_app/modules/recipe/data/data_sources/interfaces/i_search_recipe_data_source.dart';
 import 'package:chefio_recipe_app/modules/recipe/data/models/recipe.model.dart';
 import 'package:chefio_recipe_app/modules/recipe/data/models/search_filter_request.model.dart';
