89d104254ed156e93136c0da6c0d82d03177e285
vinc <appleyardvincent@gmail.com>
2024-09-03 07:45:18 +0100

    feat: Refactor RecipeGridItemComponent to use StatefulWidget for better performance

--- lib/modules/recipe/view/grid/item/recipe_grid_item.component.dart
+++ lib/modules/recipe/view/grid/item/recipe_grid_item.component.dart
@@ -32,100 +32,14 @@ class RecipeGridItemComponent extends StatelessWidget {
   }
 }
 
-class _RecipeGridItem extends StatelessWidget {
+class _RecipeGridItem extends StatefulWidget {
   const _RecipeGridItem();
 
   @override
-  Widget build(BuildContext context) {
-    final controller = context.read<RecipeGridItemController>();
-    final recipe = controller.recipe;
-    final user = recipe.user;
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            GestureDetector(
-              onTap: () {
-                context.push(ProfileScreen(user: user));
-              },
-              child: CustomCacheNetworkImage(
-                imageUrl: user.photoUrl,
-                height: 30.h,
-                width: 30.h,
-                borderRadius: BorderRadius.circular(11.r),
-              ),
-            ),
-            SizedBox(width: 8.w),
-            Expanded(
-              child: Text(
-                user.fullName.toTitleCase,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: AppText.bold500(context).copyWith(
-                  fontSize: 12.sp,
-                ),
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 16.h),
-        Stack(
-          children: [
-            GestureDetector(
-              onTap: () {
-                context.push(RecipeDetailScreen(recipe));
-              },
-              child: Hero(
-                tag: recipe.id,
-                child: CustomCacheNetworkImage(
-                  imageUrl: recipe.coverPhotoUrl,
-                  height: 151.h,
-                  width: 151.h,
-                  borderRadius: BorderRadius.circular(16.r),
-                ),
-              ),
-            ),
-            const _FavouriteButton(),
-          ],
-        ),
-        SizedBox(height: 16.h),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            recipe.title.toTitleCase,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: AppText.bold700(context).copyWith(
-              color: AppColors.headlineText,
-              fontSize: 17.sp,
-            ),
-          ),
-        ),
-        SizedBox(height: 8.h),
-        Text(
-          controller.subtitle,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: AppText.bold500(context).copyWith(
-            fontSize: 12.sp,
-            color: AppColors.secondaryText,
-          ),
-        ),
-      ],
-    );
-  }
+  State<_RecipeGridItem> createState() => _RecipeGridItemState();
 }
 
-class _FavouriteButton extends StatefulWidget {
-  const _FavouriteButton();
-
-  @override
-  State<_FavouriteButton> createState() => _FavouriteButtonState();
-}
-
-class _FavouriteButtonState extends State<_FavouriteButton> {
+class _RecipeGridItemState extends State<_RecipeGridItem> {
   void likeRecipe(Recipe recipe) async {
     try {
       await context.read<RecipeGridItemController>().likeRecipe(recipe);
@@ -143,34 +57,124 @@ class _FavouriteButtonState extends State<_FavouriteButton> {
     return Consumer<RecipeGridItemController>(
       builder: (_, controller, __) {
         final recipe = controller.recipe;
+        final user = recipe.user;
 
-        return Positioned(
-          top: 16.h,
-          right: 16.w,
-          child: GestureDetector(
-            onTap: () => likeRecipe(recipe),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.r),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
-                child: Container(
-                  width: 32.w,
-                  height: 32.h,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(8.r),
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    context.push(ProfileScreen(user: user));
+                  },
+                  child: CustomCacheNetworkImage(
+                    imageUrl: user.photoUrl,
+                    height: 30.h,
+                    width: 30.h,
+                    borderRadius: BorderRadius.circular(11.r),
                   ),
-                  child: Icon(
-                    recipe.isLiked ? Icons.favorite : Icons.favorite_border_rounded,
-                    size: 20.sp,
-                    color: recipe.isLiked ? AppColors.secondary : Colors.white,
+                ),
+                SizedBox(width: 8.w),
+                Expanded(
+                  child: Text(
+                    user.fullName.toTitleCase,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: AppText.bold500(context).copyWith(
+                      fontSize: 12.sp,
+                    ),
                   ),
+                ),
+              ],
+            ),
+            SizedBox(height: 16.h),
+            Stack(
+              children: [
+                GestureDetector(
+                  onTap: () async {
+                    await context.push(RecipeDetailScreen(recipe));
+                    setState(() {});
+                  },
+                  child: Hero(
+                    tag: recipe.id,
+                    child: CustomCacheNetworkImage(
+                      imageUrl: recipe.coverPhotoUrl,
+                      height: 151.h,
+                      width: 151.h,
+                      borderRadius: BorderRadius.circular(16.r),
+                    ),
+                  ),
+                ),
+                _FavouriteButton(
+                  recipe: recipe,
+                  onLike: () => likeRecipe(recipe),
+                ),
+              ],
+            ),
+            SizedBox(height: 8.h),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                recipe.title.toTitleCase,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: AppText.bold700(context).copyWith(
+                  color: AppColors.headlineText,
+                  fontSize: 14.sp,
                 ),
               ),
             ),
-          ),
+            SizedBox(height: 8.h),
+            Text(
+              controller.subtitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: AppText.bold500(context).copyWith(
+                fontSize: 12.sp,
+                color: AppColors.secondaryText,
+              ),
+            ),
+          ],
         );
       },
+    );
+  }
+}
+
+class _FavouriteButton extends StatelessWidget {
+  const _FavouriteButton({required this.recipe, required this.onLike});
+
+  final Recipe recipe;
+  final Function() onLike;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 16.h,
+      right: 16.w,
+      child: GestureDetector(
+        onTap: onLike,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8.r),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 8.0, sigmaY: 8.0),
+            child: Container(
+              width: 32.w,
+              height: 32.h,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(8.r),
+              ),
+              child: Icon(
+                recipe.isLiked ? Icons.favorite : Icons.favorite_border_rounded,
+                size: 20.sp,
+                color: recipe.isLiked ? AppColors.secondary : Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
