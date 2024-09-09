1b9d21a3ab499aab8d394c9d24d8823ecd721dc3
vinc <appleyardvincent@gmail.com>
2024-09-09 06:01:25 +0100

    Refactor dev.locator.dart to include FakeUserDataSource and IUserDataSource

--- lib/config/locator/dev.locator.dart
+++ lib/config/locator/dev.locator.dart
@@ -5,6 +5,8 @@ import 'package:chefio_recipe_app/modules/auth/data/data_sources/sign_in/fake_si
 import 'package:chefio_recipe_app/modules/auth/data/data_sources/sign_in/i_sign_in_data_source.dart';
 import 'package:chefio_recipe_app/modules/auth/data/data_sources/sign_up/fake_sign_up_data_source.dart';
 import 'package:chefio_recipe_app/modules/auth/data/data_sources/sign_up/i_sign_up_data_source.dart';
+import 'package:chefio_recipe_app/modules/profile/data/data_sources/fake_user_data_source.dart';
+import 'package:chefio_recipe_app/modules/profile/data/data_sources/i_user_data_source.dart';
 import 'package:chefio_recipe_app/modules/recipe/data/data_sources/fakes/fake_recipe_list_data_source.dart';
 import 'package:chefio_recipe_app/modules/recipe/data/data_sources/fakes/fake_search_recipe_data_source.dart';
 import 'package:chefio_recipe_app/modules/recipe/data/data_sources/interfaces/i_recipe_list_datasource.dart';
@@ -22,19 +24,28 @@ void initContainers({required AppConfig appConfig}) async {
   devLocator.registerLazySingleton<IRecipeDataSource>(
     () => FakeRecipeDataSource(),
   );
+
   devLocator.registerLazySingleton<IRecipeListDataSource>(
     () => FakeRecipeListDataSource(),
   );
+
   devLocator.registerLazySingleton<ISearchRecipeDataSource>(
     () => FakeSearchRecipeDataSource(),
   );
+
   devLocator.registerLazySingleton<ISignInDataSource>(
     () => FakeSignInDataSource(),
   );
+
   devLocator.registerLazySingleton<ISignUpDataSource>(
     () => FakeSignUpDataSource(),
   );
+
   devLocator.registerLazySingleton<IForgotPasswordDataSource>(
     () => FakeForgotPasswordDataSource(),
+  );
+  
+  devLocator.registerLazySingleton<IUserDataSource>(
+    () => FakeUserDataSource(),
   );
 }
