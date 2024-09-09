e3d3ff7b9d26d48721a70b142f8119d45913ca6a
vinc <appleyardvincent@gmail.com>
2024-09-09 03:50:50 +0100

    Refactor import statements in firebase_utils.dart and update User entity path

--- lib/core/services/firebase_utils.dart
+++ lib/core/services/firebase_utils.dart
@@ -3,31 +3,16 @@ import 'package:chefio_recipe_app/core/models/failure.dart';
 import 'package:chefio_recipe_app/core/services/firestore_collections.dart';
 import 'package:chefio_recipe_app/modules/auth/data/models/user.model.dart';
 import 'package:chefio_recipe_app/modules/auth/domain/entities/user.dart';
-import 'package:chefio_recipe_app/utils/functions.dart';
 
 abstract class FirebaseUtils {
   static Future<List<String>> getLikedRecipeIds(User user) async {
-    try {
-      final userDoc = await usersCollection.doc(user.id).get();
-      return List<String>.from(userDoc.data()?['liked_recipes'] ?? []);
-    } catch (e, s) {
-      logError(e, s,
-          source: './lib/modules/recipe/data/data_sources/impls/recipe_list_data_source.dart');
-
-      throw InternalFailure();
-    }
+    final userDoc = await usersCollection.doc(user.id).get();
+    return List<String>.from(userDoc.data()?['liked_recipes'] ?? []);
   }
 
   static Future<List<String>> getFollowingUserIds() async {
-    try {
-      final userDoc = await usersCollection.doc(AppSession.authUser?.id).get();
-      return List<String>.from(userDoc.data()?['following'] ?? []);
-    } catch (e, s) {
-      logError(e, s,
-          source: './lib/modules/recipe/data/data_sources/impls/recipe_list_data_source.dart');
-
-      throw InternalFailure();
-    }
+    final userDoc = await usersCollection.doc(AppSession.authUser?.id).get();
+    return List<String>.from(userDoc.data()?['following'] ?? []);
   }
 
   static Future<UserModel> getUser(String userId) async {
